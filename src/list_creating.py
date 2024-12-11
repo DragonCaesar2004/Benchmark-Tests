1f2e0436f4c82075cfb646e9b58c21b63691bb04
Timur <tmrgmdv2004@gmail.com>
2024-12-11 15:00:10 +0300

    added ListCreator class

---
+++ src/list_creating.py
@@ -0,0 +1,22 @@
+import numpy as np
+
+
+class ListCreator:
+    """Класс ListCreator предоставляет методы для создания списка целых чисел от 0 до указанной длины."""
+
+    def create_with_list_comprehensions(self, lenght: int) -> list[int]:
+        """Создает список с элементами от 0 до числа lenght - 1 с помощью списковых включений."""
+        return [                    # noqa: C416 чтобы ruff не ругался на list comprehensions
+            i for i in range(lenght)
+        ]
+
+    def create_with_for_cycle(self, lenght: int) -> list[int]:
+        """Создает список с элементами от 0 до числа lenght - 1 в цикле for, добавляя элементы в конец."""
+        result_list = []
+        for elem in range(lenght):
+            result_list.append(elem)  # noqa: PERF402 чтобы ruff не ругался на append
+        return result_list
+
+    def create_with_numpy(self, lenght: int) -> list[int]:
+        """Создает список с элементами от 0 до числа lenght - 1 с помощью библиотеки numpy."""
+        return np.arange(lenght).tolist()
